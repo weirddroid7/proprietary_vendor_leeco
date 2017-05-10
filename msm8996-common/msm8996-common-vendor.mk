2653415a5b5a9cf83a8f8ef14f9f29b8f2637a91
Marko Man <darkobas@gmail.com>
2017-05-10 10:20:46 +0200

    zl1: remove qcom location provider
    Change-Id: I4e225b0b5cdbfa3924de2422e29825c0adcf8b41

--- msm8996-common/msm8996-common-vendor.mk
+++ msm8996-common/msm8996-common-vendor.mk
@@ -404,7 +404,6 @@ PRODUCT_PACKAGES += \
     shutdownlistener \
     CNEService \
     QtiTetherService \
-    com.qualcomm.location \
     dpmserviceapp \
     qcrilmsgtunnel \
     colorservice \
