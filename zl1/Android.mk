7e159091c89b414167ebe4e25ec4e7cd925eaa27
mosimchah <mosimchah@gmail.com>
2017-10-09 14:11:21 -0400

    Revert "zl1: add stock ir app", broken on oreo
    This reverts commit 6a8f64e56d420581e60d5253c2328b4e27638dfb.
    
    Change-Id: If89bf0def059f33e3ace7ce1fe83385e4f279916

--- zl1/Android.mk
+++ zl1/Android.mk
@@ -46,26 +46,4 @@ LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := UEIQuicksetSDKLeTV
-LOCAL_MODULE_OWNER := Letv
-LOCAL_SRC_FILES := proprietary/app/UEIQuicksetSDKLeTV/UEIQuicksetSDKLeTV.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := LetvRemoteControl
-LOCAL_MODULE_OWNER := Letv
-LOCAL_SRC_FILES := proprietary/app/LetvRemoteControl/LetvRemoteControl.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
 endif
